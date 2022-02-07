675139251f11b81d1ae1eef8558dc745855682bd
ivan Kotov <ivan.kotov@2011.auditory.ru>
2022-02-07 17:23:22 +0300

    complete task_3

--- Kotov_Ivan_dz_3/task_3.py
+++ Kotov_Ivan_dz_3/task_3.py
@@ -1,7 +1,12 @@
 def thesaurus(*args) -> dict:
-
+    '''Формирует словарь, в котором ключи — первые буквы имён, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы.
+    '''
     dict_out = {}
-    
+    for word in args:
+        dict_value = dict_out.setdefault(word[0], list())
+        if word not in dict_value:
+            dict_value.append(word)
+            dict_out[word[0]] = dict_value
     return dict_out
 
 
