72bba9569d6c306556f109b7a4b9c41ce9865aa6
Ivan Kotov <ivan.kotov@2011.auditory.ru>
2022-02-12 19:12:58 +0300

    comlete task_1

--- Kotov_Ivan_dz_5/task_1.py
+++ Kotov_Ivan_dz_5/task_1.py
@@ -1,10 +1,11 @@
 def odd_nums(number: int) -> int:
     """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
-    pass  # пишите свою реализацию здесь
+    for n in range(1, number + 1, 2):
+        yield n
 
 
 n = 15
 generator = odd_nums(n)
 for _ in range(1, n + 1, 2):
     print(next(generator))
-# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
+next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
